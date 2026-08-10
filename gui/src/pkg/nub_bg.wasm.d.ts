de48bbd5951f35885d01e6d22933409cb23894ce
Mvzvrt <87718155+Mvzvrt@users.noreply.github.com>
2026-08-11 02:47:27 +0800

    chore: for vercel deployment

---
+++ gui/src/pkg/nub_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_damathwasmengine_free: (a: number, b: number) => void;
+export const damathwasmengine_find_best_move: (a: number, b: number, c: bigint) => [number, number, number];
+export const damathwasmengine_get_legal_moves: (a: number) => [number, number, number];
+export const damathwasmengine_get_state: (a: number) => [number, number, number];
+export const damathwasmengine_make_move: (a: number, b: number, c: number, d: number, e: number) => [number, number, number];
+export const damathwasmengine_new: () => number;
+export const damathwasmengine_reset: (a: number) => [number, number, number];
+export const init_panic_hook: () => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
